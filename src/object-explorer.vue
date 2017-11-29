7c67d94d77556f4bd04a0691698fc66eb86011a0
David Nowinsky <david@nowinsky.net>
2017-11-29 14:57:53 +0100

    Illustration of the ref update problem

--- src/object-explorer.vue
+++ src/object-explorer.vue
@@ -43,7 +43,7 @@ export default {
   },
   methods: {
     updatePropertyValue (propertyName, newValue) {
-      this.obj[propertyName] = newValue;
+      this.obj = Object.assign({}, this.obj, {[propertyName]: newValue});
     }
   }
 }
